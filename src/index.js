162d0c69834022a11dcb421a86af45734a277b5e
ShivtejTakawale <72022478+ShivtejTakawale@users.noreply.github.com>
2021-03-24 02:51:01 +0530

    Fourth commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import React, { Component } from 'react';
- import PropTypes from 'prop-types';
+
 
 
 ReactDOM.render(
